8bcf470ac2420950ab19f8dfccc58a3f73d0dd5f
stivenkno <109120233+stivenkno@users.noreply.github.com>
2024-07-05 00:40:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
+ * Configuración del servidor Express con CORS y manejo de rutas para una API de medios.
+ * 
+ * Dependencias:
+ * - express: Framework web minimalista para Node.js.
+ * - cors: Middleware para habilitar CORS.
+ * - dotenv: Carga variables de entorno desde un archivo .env.
+ * 
+ * Rutas:
+ * - /api/movies: Maneja rutas relacionadas con películas.
+ * - /api/tv: Maneja rutas relacionadas con programas de televisión.
+ * - /api/people: Maneja rutas relacionadas con personas.
+ * - /api/trending: Maneja rutas relacionadas con contenidos en tendencia.
+ * 
+ * El servidor escucha en el puerto especificado en las variables de entorno.
+ */
+
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
@@ -14,11 +31,12 @@ dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/movies', movies)
-app.use('/api/tv', tv)
-app.use('/api/people', people)
-app.use('/api/trending', trending)
+app.use('/api/movies', movies)    // Rutas relacionadas con películas
+app.use('/api/tv', tv)            // Rutas relacionadas con programas de TV
+app.use('/api/people', people)    // Rutas relacionadas con personas
+app.use('/api/trending', trending) // Rutas relacionadas con contenidos en tendencia
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
+  console.log(`El servidor está corriendo en el puerto ${process.env.PORT}`)
 })
+
